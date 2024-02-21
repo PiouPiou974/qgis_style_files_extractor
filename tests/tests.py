707c54f90ed7a15edb634483511a6d2f0ad630d1
Antoine <antoine.petit@lilo.org>
2024-02-21 20:27:45 +0400

    21/02

--- tests/tests.py
+++ tests/tests.py
@@ -4,7 +4,7 @@ from qgis_style_files_extractor.main import QmlToStyles
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        qml_path = 'qml_samples\\servitudes_d_utilité_publique_s.qml'
+        qml_path = 'qml_samples\\test.qml'
         styles = QmlToStyles(qml_path)
         print(styles.rules)
         self.assertEqual(True, True)  # add assertion here
